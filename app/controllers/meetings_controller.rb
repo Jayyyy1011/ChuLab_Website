e11cc7c2e1317e4fdc054f2a1cc2f4f5cbbb005f
laijay <jack1011tw@gmail.com>
2020-08-13 14:00:51 +0800

    meeting create action

--- app/controllers/meetings_controller.rb
+++ app/controllers/meetings_controller.rb
@@ -4,4 +4,24 @@ class MeetingsController < ApplicationController
     @meetings = Meeting.all
   end
 
+  def new
+    @meeting = Meeting.new
+  end
+
+  def create
+    @meeting = Meeting.new(meeting_params)
+    if @meeting.save
+      redirect_to meetings_path
+    else
+      render :new
+    end
+  end
+
+
+  private
+
+  def meeting_params
+    params.require(:meeting).permit(:date, :speaker, :remark)
+  end
+
 end
