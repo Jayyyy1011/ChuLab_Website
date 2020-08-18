60b704ac323eee457a566c8364998758d9b8fe04
laijay <jack1011tw@gmail.com>
2020-08-18 20:36:31 +0800

    paginate posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,7 +3,7 @@ class PostsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destroy]
 
   def index
-    @posts = Post.recent
+    @posts = Post.recent.paginate(:page => params[:page], :per_page => 4)
   end
 
   def new
