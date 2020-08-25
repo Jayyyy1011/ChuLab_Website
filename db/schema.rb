deacd9a7f32c7e154c14855de9ff56548161e107
laijay <jack1011tw@gmail.com>
2020-08-25 17:42:38 +0800

    delete profile

--- db/schema.rb
+++ db/schema.rb
@@ -73,15 +73,6 @@ ActiveRecord::Schema.define(version: 20200825085430) do
     t.string   "title"
   end
 
-  create_table "profiles", force: :cascade do |t|
-    t.integer  "user_id"
-    t.string   "name"
-    t.date     "birthday"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["user_id"], name: "index_profiles_on_user_id"
-  end
-
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "",    null: false
     t.string   "encrypted_password",     default: "",    null: false
